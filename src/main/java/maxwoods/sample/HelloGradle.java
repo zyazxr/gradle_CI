d0129b320cd50e5190947da2950fc3af1da30eca
zhaoyu <15991604590@139.com>
2018-05-31 11:30:29 +0800

    commit ci travis ci add 2

--- src/main/java/maxwoods/sample/HelloGradle.java
+++ src/main/java/maxwoods/sample/HelloGradle.java
@@ -3,5 +3,6 @@ package maxwoods.sample;
 public class HelloGradle {
     public static void main(String[] args) {
         System.out.print("Hello,Gradle####################!");
+        System.out.print("this is ok out put");
     }
 }
